0c2123bb81e78a8c597046a86cb7c46c1b249a2a
Yoni Satat <yoni.satat@gmail.com>
2017-12-31 20:46:56 +0000

    added dropdown for employment experience section

--- client/public/bundle.js
+++ client/public/bundle.js
@@ -60,71 +60,119 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 1);
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
 /******/ })
 /************************************************************************/
 /******/ ([
-/* 0 */,
-/* 1 */
+/* 0 */
 /***/ (function(module, exports, __webpack_require__) {
 
-
+const Request = __webpack_require__(1);
 const ResumeView = __webpack_require__(2);
 
-
+const resumeView = new ResumeView();
+const requestResume = new Request("http://localhost:3000/api/resume");
 
 
 const app = function() {
-  console.log("hello world");
+  const select = document.querySelector('#job-title');
+
+  requestResume.get(function(jobs) {
+    jobs.forEach(function(job) {
+      const option = document.createElement('option');
+      option.innerText = job.employer;
+      // option.value = index;
+      select.appendChild(option);
+      // requestResume.post(createResumeRequestCompleted, job);
+    });
+  });
+
+  const createResumeRequestCompleted = function(job) {
+    resumeView.addJob(job);
+  }
+
+
+  console.log("Last line in app.js");
 }
 document.addEventListener('DOMContentLoaded', app);
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports) {
+
+const Request = function(url) {
+  this.url = url;
+}
+
+Request.prototype.get = function(callback) {
+  const request = new XMLHttpRequest();
+  request.open('GET', this.url);
+  request.addEventListener('load', function() {
+    if(this.status !== 200) {
+      return;
+    }
+    const responseBody = JSON.parse(this.responseText);
+    callback(responseBody);
+  });
+  request.send();
+}
+
+Request.prototype.post = function(callback, body) {
+  const request = new XMLHttpRequest();
+  request.open('POST', this.url);
+  request.setRequestHeader('Content-Type', 'application/json');
+  request.addEventListener('load', function() {
+    console.log('Yoni wants to log');
+    if(this.status !== 201) {
+      return;
+    }
+    const responseBody = JSON.parse(this.responseText);
+    console.log(responseBody);
+    callback(responseBody);
+  });
+  console.log('body',body);
+  request.send(JSON.stringify(body));
+}
+
+Request.prototype.delete = function(callback) {
+  const request = new XMLHttpRequest();
+  request.open('DELETE', this.url);
+  request.addEventListener('load', function() {
+    if(this.status !== 204) {
+      return;
+    }
+    callback();
+  });
+  request.send();
+}
+
+module.exports = Request;
 
 
 /***/ }),
 /* 2 */
 /***/ (function(module, exports) {
 
-// const Resume = require('../models/resume.js');
-//
-// const resume = new Resume([
-//   {
-//     "employer": "British Gas, Edinburgh",
-//     "role": "Customer Service Agent",
-//     "description": "Working with pay as you go customers over the phone to resolve issues with their service such as lost top-up cards or keys, bill queries, faulty meters and refunds. Creating sales opportunities for the sales team to achieve business growth.",
-//     "date": "May 2016 - October 2016"
-//   },
-//   {
-//     "employer": "Lloyds Banking Group, Edinburgh",
-//     "role": "Customer Service Agent",
-//     "description": "Serving Retail Business Banking customers from all of the UK over the phone. Liaising between customers and the bank to achieve the best and fairest outcome for the customers. Being the First contact for the customers to help & assist with their day to day tasks such as making payments, setting up new payments, stopping cheque etc. Promoting other bank services to meet customer's needs. Logging complaints for customers and work to get full resolution.  Assisting Team Coaches and  Line Manager with training for new colleagues.  Health & Safety rep and stationery orders on a voluntary basis.",
-//     "date": "2014 -  April 2016"
-//   },
-//   {
-//     "employer": "Three Birds Restaurant, Edinburgh",
-//     "role": "Sous Chef",
-//     "description": "Assistant manager to head chef, Running the kitchen on a day to day basis and reporting to the head chef and owners. Ordering, food cost management, HACCP sheets generated daily. Help source suppliers and build good relationships with them.  Creating menu specials on a day to day basis.",
-//     "date": "2012 - 2014"
-//   },
-//   {
-//     "employer": "The Dogs Restaurant, Edinburgh",
-//     "role": "Sous Chef",
-//     "description": "(Moved to Edinburgh due to spouses enrolment in a three year PhD program at Edinburgh University). Started as a Commis Chef eventually being to promoted to Head Chef. Ordering, food cost management, HACCP generated daily. Supplier Liaisons and Negotiations. Setting up seasonal menu changes and weekly specials. Training junior members of the kitchen and trainees from JET and Right Track training programme Awards received while working in the dogs: Michelin Bib Gourmand 2009, Best value restaurant in the UK from the Which Good Food Guide 2010, Placed No. 64 out of 100 top UK restaurants in the National Restaurant Awards 2010.",
-//     "date": "2009 - 2012"
-//   },
-//   {
-//     "employer": "Tel Yosef Hummus Shop, Israel",
-//     "role": "Owner Manager",
-//     "description": "Ground Up restaurant creation including Location, menu design, restaurant layout, etc.  Pricing GP and cash flow management.  Sourcing suppliers. Recruitment, training staff. Advertising and promotion business management. Able to run any area in the restaurant.",
-//     "date": "2007 - 2009"
-//   },
-//   {
-//     "employer": "Grafikal Screen Printing and Merchandise, Israel",
-//     "role": "Owner Manager",
-//     "description": "Managing a team of 10 employees. Sourcing new suppliers, build and maintain strong relationship with them.  Customers service.  Cash flow management and payment collections.  Planning and Building marketing strategies and advertising to support company sales. Recruitment for all sections of the company. Able to fill in any section in the company including IT, printing, setting up orders etc.",
-//     "date": "1996 - 2009"
-//   }
-//
-// ]);
+const ResumeView = function() {
+  this.jobs = [];
+}
+
+ResumeView.prototype.addJob = function(job) {
+  this.jobs.push(job);
+  console.log(this.jobs);
+}
+
+// ResumeView.prototype.populateDropDown = function(job) {
+//   const select = document.querySelector('#job-title');
+//     jobs.forEach(function(job) {
+//       const option = document.createElement('option');
+//       option.innerText = job.role;
+//       select.appendChild(option);
+//     });
+// }
+
+module.exports = ResumeView;
 
 
 /***/ })
--- client/public/src/app.js
+++ client/public/src/app.js
@@ -1,10 +1,28 @@
-
+const Request = require('./services/request.js');
 const ResumeView = require('./views/resume_view.js');
 
-
+const resumeView = new ResumeView();
+const requestResume = new Request("http://localhost:3000/api/resume");
 
 
 const app = function() {
-  console.log("hello world");
+  const select = document.querySelector('#job-title');
+
+  requestResume.get(function(jobs) {
+    jobs.forEach(function(job) {
+      const option = document.createElement('option');
+      option.innerText = job.employer;
+      // option.value = index;
+      select.appendChild(option);
+      // requestResume.post(createResumeRequestCompleted, job);
+    });
+  });
+
+  const createResumeRequestCompleted = function(job) {
+    resumeView.addJob(job);
+  }
+
+
+  console.log("Last line in app.js");
 }
 document.addEventListener('DOMContentLoaded', app);
