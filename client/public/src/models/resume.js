f74b6d38b1116f8ece0d3092e19e8f332599f747
Yoni Satat <yoni.satat@gmail.com>
2017-12-31 18:07:24 +0000

    comented out resume.js and resume_view.js

--- client/public/src/models/resume.js
+++ client/public/src/models/resume.js
@@ -1,11 +1,11 @@
-const Resume = function(options) {
-  this.empEx = [];
-}
-
-Resume.prototype.getEmployer = function() {
-  resume.forEach(function(employer) {
-    console.log(employer);
-  });
-}
-
-module.exports = Resume;
+// const Resume = function(options) {
+//   this.empEx = [];
+// }
+//
+// Resume.prototype.getEmployer = function() {
+//   resume.forEach(function(employer) {
+//     console.log(employer);
+//   });
+// }
+//
+// module.exports = Resume;
